b468eb251920d4fcf61119f3e1bb68088168c6be
mandil01 <juan.castro03@iest.edu.mx>
2026-05-08 08:44:53 -0600

    Add files via upload

---
+++ repasotresh/repasotresh/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Dim cont As String = 0
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        cont += 1
+        Label2.Text = cont
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        cont = 0
+        Label2.Text = cont
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        cont += 2
+        Label2.Text = cont
+    End Sub
+End Class
---
+++ repasotresh/repasotresh/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("repasotresh")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("repasotresh")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("766a2e86-b5dc-4239-b294-3c638d23a0b1")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+' mediante el carácter '*', como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
